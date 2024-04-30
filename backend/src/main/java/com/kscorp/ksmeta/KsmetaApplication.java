2c1d9d581af2cb95688055f7d7a763e212c357dc
k-Sidney <kleubersidney@hotmail.com>
2024-04-30 20:28:29 -0300

    Project created

---
+++ backend/src/main/java/com/kscorp/ksmeta/KsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.kscorp.ksmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kscorp/ksmeta/KsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kscorp.ksmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
